d5f2016936d5c66ab721e00dfe7d53cb68cd97cd
Jonathan <jonemerytech@gmail.com>
2022-06-30 09:31:33 -0400

    testing workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/fivetool',
-  assetPrefix: '/fivetool',
   reactStrictMode: true,
   images: {
     domains: ['files.stripe.com'],
